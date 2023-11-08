3945ed73586724228036c88b8c17ba7c77df87f6
Noa Ferman <noaf7@uw.edu>
2023-11-08 15:05:09 -0800

    removed uneeded files

--- lab6/build/CMakeFiles/install-DivZeroPass.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-DivZeroPass"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-DivZeroPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()

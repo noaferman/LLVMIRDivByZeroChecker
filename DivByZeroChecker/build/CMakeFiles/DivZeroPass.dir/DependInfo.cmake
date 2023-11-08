186f0d835ccc2631c2f7f71ef7aa60509b62c6a7
Noa Ferman <noaf7@uw.edu>
2023-11-08 15:04:43 -0800

    build files?

---
+++ DivByZeroChecker/build/CMakeFiles/DivZeroPass.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/DivByZeroChecker/src/ChaoticIteration.cpp" "/DivByZeroChecker/build/CMakeFiles/DivZeroPass.dir/src/ChaoticIteration.cpp.o"
+  "/DivByZeroChecker/src/DivZeroAnalysis.cpp" "/DivByZeroChecker/build/CMakeFiles/DivZeroPass.dir/src/DivZeroAnalysis.cpp.o"
+  "/DivByZeroChecker/src/Domain.cpp" "/DivByZeroChecker/build/CMakeFiles/DivZeroPass.dir/src/Domain.cpp.o"
+  "/DivByZeroChecker/src/Transfer.cpp" "/DivByZeroChecker/build/CMakeFiles/DivZeroPass.dir/src/Transfer.cpp.o"
+  "/DivByZeroChecker/src/Utils.cpp" "/DivByZeroChecker/build/CMakeFiles/DivZeroPass.dir/src/Utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GTEST_HAS_RTTI=0"
+  "__STDC_CONSTANT_MACROS"
+  "__STDC_FORMAT_MACROS"
+  "__STDC_LIMIT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/lib/llvm-8/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ DivByZeroChecker/build/CMakeFiles/DivZeroPass.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DivZeroPass.dir/src/DivZeroAnalysis.cpp.o"
+  "CMakeFiles/DivZeroPass.dir/src/Transfer.cpp.o"
+  "CMakeFiles/DivZeroPass.dir/src/ChaoticIteration.cpp.o"
+  "CMakeFiles/DivZeroPass.dir/src/Domain.cpp.o"
+  "CMakeFiles/DivZeroPass.dir/src/Utils.cpp.o"
+  "DivZeroPass.pdb"
+  "DivZeroPass.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DivZeroPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
